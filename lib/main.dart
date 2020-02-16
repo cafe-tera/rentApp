b573b729e8bf075b6802cf096cf0a774ffdf6444
cafe-tera <jorge.eff.96@gmail.com>
2020-02-15 23:54:54 -0300

    cambios en el mian

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,6 @@ import 'package:flutter/material.dart';
 // local imports
 import 'package:rent_app/src/routes/routes.dart';
 import 'package:rent_app/src/pages/logInUp_page.dart';
-
-import 'package:rent_app/src/bloc/provider.dart';
-import 'package:rent_app/src/bloc/mis_domicilios_bloc/mis_domicilios_bloc.dart';
 //--------------------------------------------------------------------------------------------------------------------
 
 void main() => runApp(MyApp());
@@ -18,19 +15,14 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final MisDomiciliosBloc misDomiciliosBloc = MisDomiciliosBloc();
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<MisDomiciliosBloc>(
-        bloc: misDomiciliosBloc,
-        onDispose: (BuildContext context, MisDomiciliosBloc bloc) =>
-            bloc.dispose(),
-        child: MaterialApp(
+    return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Material App',
           initialRoute: LogInUpPage.routeName,
           routes: getApplicationRoutes(),
-        ));
+        );
   }
 }
